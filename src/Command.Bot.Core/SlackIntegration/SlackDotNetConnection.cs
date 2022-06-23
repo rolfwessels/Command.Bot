173480d2d9974e7b227ac5286c86064eb020e5b5
Rolf <rolf.wessels@gmail.com>
2022-06-23 17:18:07 +0200

    Try getting while typing working.

--- src/Command.Bot.Core/SlackIntegration/SlackDotNetConnection.cs
+++ src/Command.Bot.Core/SlackIntegration/SlackDotNetConnection.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -16,12 +15,13 @@ namespace Command.Bot.Core.SlackIntegration
         private static readonly ILogger _log = Log.ForContext(MethodBase.GetCurrentMethod().DeclaringType);
         private readonly string _key;
         private bool _isStarted;
-        private SlackBot _bot;
 
         public SlackDotNetConnection(string key)
         {
             _key = key;
         }
+
+        public SlackBot Bot { get; private set; }
 
         public async Task Start(ISlackConnectionHandler slackConnectionHandler)
         {
@@ -29,22 +29,20 @@ namespace Command.Bot.Core.SlackIntegration
             {
                 _isStarted = true;
                 _log.Information($"Starting {_key}");
-                _bot = new SlackBot(_key);
-                _bot.AddHandler(new MyMessageHandler(slackConnectionHandler,this));
-                
-                await _bot.Connect();
+                Bot = new SlackBot(_key);
+                Bot.AddHandler(new MyMessageHandler(slackConnectionHandler, this));
+
+                await Bot.Connect();
             }
         }
-
-        public SlackBot Bot => _bot;
 
         public void Stop()
         {
             if (_isStarted)
             {
                 _isStarted = false;
-                _bot.Dispose();
-                _bot = null;
+                Bot.Dispose();
+                Bot = null;
             }
         }
 
@@ -63,21 +61,21 @@ namespace Command.Bot.Core.SlackIntegration
             {
                 await _handler.MessageReceived(new Request(message, _slackDotNetConnection));
             }
-
-            
         }
 
         public class Request : ISlackRequest
         {
-            private readonly IMessage _message;
             private readonly SlackDotNetConnection _connection;
+            private readonly IMessage _message;
 
             public Request(IMessage message, SlackDotNetConnection connection)
             {
                 _message = message;
                 _connection = connection;
                 Detail = new DetailContextDetail(message);
-            }   
+            }
+
+            public ISlackDetail Detail { get; }
 
             public class DetailContextDetail : ISlackDetail
             {
@@ -88,58 +86,47 @@ namespace Command.Bot.Core.SlackIntegration
                     UserId = message.User.Id;
                 }
 
-                #region Implementation of ISlackMessage
-
                 public string UserName { get; }
                 public string Text { get; }
                 public string UserId { get; }
-
-                #endregion
             }
-
-
-            #region Implementation of ISlackMessageContext
-
-            public ISlackDetail Detail { get; }
 
             public bool IsForBot()
             {
-             return !_message.User.IsBot &&  (_message.MentionsBot || _message.Conversation.IsPrivate);
+                return !_message.User.IsBot && (_message.MentionsBot || _message.Conversation.IsPrivate);
             }
 
             public async Task Reply(ReplyMessage message)
             {
-                var botMessage = new BotMessage() {
-                    Text = message.Text ,
+                var botMessage = new BotMessage
+                {
+                    Text = message.Text,
                     Conversation = _message.Conversation,
-                    Attachments = message.Attachments?.Select(x=>new Attachment() {Text = x.Text,Color = x.ColorHex}).ToList()
+                    Attachments = message.Attachments?.Select(x => new Attachment { Text = x.Text, Color = x.ColorHex })
+                        .ToList()
                 };
-                
-                await _connection._bot.Send(botMessage);
+
+                await _connection.Bot.Send(botMessage);
             }
 
             public Task IndicateTyping()
             {
-                _connection._bot.WhileTyping(_message.Conversation.Id,()=> Task.Delay(1000));
-                return Task.CompletedTask;
+                var indicateTyping = Task.Delay(100);
+                _connection.Bot.WhileTyping(_message.Conversation.Id, () => indicateTyping);
+                return indicateTyping;
             }
-
-            #endregion
         }
-        #region IDisposable
 
         public void Dispose()
         {
             Stop();
         }
 
-        #endregion
-
         public async Task SayToUser(string userName, string message)
         {
-            var user = await _bot.GetUserByName(userName);
-            var conversationByUserId = await _bot.GetConversationByUserId(user.Id);
-            await _bot.Send(new BotMessage() { Conversation = conversationByUserId, Text = message });
+            var user = await Bot.GetUserByName(userName);
+            var conversationByUserId = await Bot.GetConversationByUserId(user.Id);
+            await Bot.Send(new BotMessage { Conversation = conversationByUserId, Text = message });
         }
     }
 }
